baf8d87454063e05c938503311afcc7cc1a3d2f1
TCHBG <146738223+TMHBGYT@users.noreply.github.com>
2024-04-07 11:34:22 +0200

    eeeeee

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,11 @@ Attribute.add("damiud-fragment");
 Events.on(ContentInitEvent, e => {
     Vars.content.block("project-big-damiud-env-wall").attributes.set(Attribute.get("damiud-fragment"), 1);
 });
+Attribute.add("oxygen");
+Events.on(ContentInitEvent, e => {
+    Vars.content.block("project-big-pionide-floor-oxygen").attributes.set(Attribute.get("oxygen"), 1);
+});
+Attribute.add("hydrogene");
+Events.on(ContentInitEvent, e => {
+    Vars.content.block("project-big-damiud-floor-hydrogene").attributes.set(Attribute.get("hydrogene"), 1);
+});
