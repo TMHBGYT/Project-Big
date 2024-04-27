dcd3f65a2843264a0156e8cad3d7484e4c9ab4b0
TCHBG <146738223+TMHBGYT@users.noreply.github.com>
2024-04-27 23:00:17 +0200

    CoLoSaL "oH nO" YiPeE (i aint spriting all of thgis tuff,someone do for meeeeeeeeeee

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 Attribute.add("damiud-fragment");
 Events.on(ContentInitEvent, e => {
-    Vars.content.block("project-big-damiud-env-wall").attributes.set(Attribute.get("damiud-fragment"), 1);
+    Vars.content.block("pb-damiud-env-wall").attributes.set(Attribute.get("damiud-fragment"), 1);
 });
 Attribute.add("oxygen");
 Events.on(ContentInitEvent, e => {
-    Vars.content.block("project-big-pionide-floor-oxygen").attributes.set(Attribute.get("oxygen"), 1);
+    Vars.content.block("pb-pionide-floor-oxygen").attributes.set(Attribute.get("oxygen"), 1);
 });
 Attribute.add("hydrogene");
 Events.on(ContentInitEvent, e => {
-    Vars.content.block("project-big-damiud-floor-hydrogene").attributes.set(Attribute.get("hydrogene"), 1);
+    Vars.content.block("pb-damiud-floor-hydrogene").attributes.set(Attribute.get("hydrogene"), 1);
 });
